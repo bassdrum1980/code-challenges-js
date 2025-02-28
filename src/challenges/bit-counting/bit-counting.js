1c9fe2cab501c0f7cf6df0875b260aaa9e8d6bfe
Sasha Shevchenko <acidi@icloud.com>
2025-02-28 12:58:55 -0800

    employs shared render function

---
+++ src/challenges/bit-counting/bit-counting.js
@@ -0,0 +1,23 @@
+import renderChallenge from "../../utils/utils";
+
+// Challenge description
+const challenge = {
+  title: "Bit Counting",
+  description: [
+    "Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.",
+    " Example: The binary representation of <code>1234</code> is <code>10011010010</code>, so the function should return <code>5</code> in this case",
+  ],
+  testCases: {
+    1234: 5,
+    4: 1,
+    10: 2,
+  },
+};
+
+// Solution
+function countBits(n) {
+  return n.toString(2).split("0").join("").length;
+}
+
+// Render the challenge 🚀
+renderChallenge(challenge, countBits, (key) => countBits(Number(key)));
